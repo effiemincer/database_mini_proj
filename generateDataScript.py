11c706fd62efa85f2c256a5c75917cb0d2c8bc65
Effie Mincer <148328567+effiemincer@users.noreply.github.com>
2024-12-01 13:20:01 +0200

    python script generating the data

---
+++ generateDataScript.py
@@ -0,0 +1,77 @@
+from faker import Faker
+import random
+
+fake = Faker()
+
+# Generate sample data for Readers
+with open("readers.sql", "w") as file:
+    for _ in range(33333):
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        address = fake.address().replace("\n", ", ")
+        phone = fake.phone_number()
+        file.write(f"INSERT INTO Readers (FirstName, LastName, Address, PhoneNumber) "
+                   f"VALUES ('{first_name}', '{last_name}', '{address}', '{phone}');\n")
+
+
+CARDTYPE = ['Physical', 'Electronic']
+# Generate sample data for ReaderCard
+with open("readerCard.sql", "w") as file:
+    for _ in range(33333):  
+        readerID = _ + 1
+        cardType = CARDTYPE[random.randint(0,1)]
+        expiryDate = fake.date_this_decade(before_today=True, after_today=True)
+        file.write(f"INSERT INTO ReaderCard (ReaderID, CardType, ExpirationDate) "
+                   f"VALUES ('{readerID}', '{cardType}', '{expiryDate}');\n")
+        
+
+RELATIONTPYE = ['Parent', 'Child', 'Spouse', 'Sibling']
+# Generate sample data for FamilyTies
+with open("familyTies.sql", "w") as file:
+    for _ in range(10000):  
+        readerID = random.randint(1, 33333) * 37 % 33333  # Randomly select a readerID
+        relationReaderID = random.randint(1, 33333) * 37 % 33333
+        while relationReaderID == readerID:
+            relationReaderID = random.randint(1, 33333) * 2 % 33333
+        cardType = CARDTYPE[random.randint(0,1)]
+        expiryDate = fake.date_this_decade(before_today=True, after_today=True)
+        file.write(f"INSERT INTO FamilyTies (ReaderID, CardType, ExpirationDate) "
+                   f"VALUES ('{readerID}', '{cardType}', '{expiryDate}');\n")
+
+
+# Generate sample data for BooksOnLoan
+with open("booksOnLoan.sql", "w") as file:
+    # loans that were fulfilled (will have the loanID in BooksReturned)
+    for _ in range(50000):  
+        readerID = random.randint(1, 33333)
+        bookID = random.randint(1, 100000)
+        loanDate = fake.date_between(start_date='-15y', end_date='-2m')
+        returnDate = fake.date_between(start_date=loanDate, end_date='today')
+        file.write(f"INSERT INTO BooksOnLoan (ReaderID, BookID, LoanDate, ReturnDate) "
+                   f"VALUES ('{readerID}', '{cardType}', '{loanDate}', '{returnDate}');\n")
+        
+    
+    # all loans are still outstanding
+    for _ in range(50000):  
+        readerID = random.randint(1, 33333)
+        bookID = random.randint(1, 100000)
+        loanDate = fake.date_between(start_date='-1m', end_date='today')
+        returnDate = fake.date_between(start_date='today', end_date='+1m')
+        file.write(f"INSERT INTO BooksOnLoan (ReaderID, BookID, LoanDate, ReturnDate) "
+                   f"VALUES ('{readerID}', '{cardType}', '{loanDate}', '{returnDate}');\n")
+        
+    
+CONDITION = ['Excellent', 'Good', 'Fair', 'Poor', 'Damaged']
+# Generate sample data for BooksReturned
+with open("booksReturned.sql", "w") as file:
+    # books that were on loan and are now returned (may be on loan again but now to a new reader)
+    for _ in range(50000):  
+        loanID = _ + 1
+        conditionOnReturn = CONDITION[random.randint(0, 4)]
+        returnDate = fake.date_between(start_date='-15y', end_date='today')
+        file.write(f"INSERT INTO BooksReturned (LoanID, CondtionOnReturn, ReturnDate) "
+                   f"VALUES ('{loanID}', '{conditionOnReturn}', '{returnDate}');\n")
+        
+
+        
+

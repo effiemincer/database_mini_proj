768c44770d868872448b4bf819cb778dae246bc7
Effie Mincer <148328567+effiemincer@users.noreply.github.com>
2024-12-01 13:11:32 +0200

    Update DB1.sql

--- DB1.sql
+++ DB1.sql
@@ -36,9 +36,6 @@ CREATE TABLE BooksOnLoan (
     FOREIGN KEY (ReaderID) REFERENCES Readers(ReaderID)
 );
 
--- Set the starting value of LoanID's sequence to 30000
-ALTER SEQUENCE books_on_loan_loanid_seq RESTART WITH 30000;
-
 -- Table for BooksReturned
 CREATE TABLE BooksReturned (
     ReturnID SERIAL PRIMARY KEY,

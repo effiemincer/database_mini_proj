7fbaf80025d1f8e81bbc20799ad6a6c691ed8b98
rseltzer123 <rachamimseltzer@gmail.com>
2024-12-02 17:09:40 +0200

    query stuff

--- ParamsQueries.sql
+++ ParamsQueries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff07aa0a3d36701c6d94cccb2a4f39d21350d05400d2deebecf99d1f74d13bd4
-size 2404
+oid sha256:a9183d32889864fb8410ee791b5aa784539b82418363e8b5b4ef2cf5d1ec3faf
+size 2511
--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bac23e1228c8220a3a9289daa7244ba3fe28d1161a063638e8abdc15393d08b6
-size 3853
+oid sha256:c83f29c3a1ae7d19c2ef4d2dfadfdbe69a95368e17d6ace028a4e387f4a9be55
+size 4189
